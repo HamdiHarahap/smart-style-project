264dd16d15e11082f398ff4fc3b388015392c5de
HamdiHarahap <hamdiharahap2005@gmail.com>
2025-05-10 11:06:41 +0700

    commit

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -15,11 +15,5 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         // User::factory(10)->create();
-
-        User::create([
-            'name' => 'Admin',
-            'email' => 'admin@gmail.com',
-            'password' => Hash::make('123')
-        ]);
     }
 }
